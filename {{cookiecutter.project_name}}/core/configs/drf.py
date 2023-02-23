2735c60cd93c01f2943027793b790b3ed8a7d311
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 15:46:02 +0330

    bugfix in drf.py

--- {{cookiecutter.project_name}}/core/configs/drf.py
+++ {{cookiecutter.project_name}}/core/configs/drf.py
@@ -1,7 +1,5 @@
 # Config DRF
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': '',
-    'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
