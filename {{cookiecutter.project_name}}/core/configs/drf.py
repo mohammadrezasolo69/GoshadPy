5a3a53808e6952449b760ab5e10f27ba0bc9556d
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 14:16:19 +0330

    write core/configs/drf

---
+++ {{cookiecutter.project_name}}/core/configs/drf.py
@@ -0,0 +1,9 @@
+# Config DRF
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': '',
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
