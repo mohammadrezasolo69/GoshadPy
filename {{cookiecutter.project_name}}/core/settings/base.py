995c38879d050b43e4e55b78bfc7bea71a721ef8
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 15:46:58 +0330

    bugfix(add assets in base.py)

--- {{cookiecutter.project_name}}/core/settings/base.py
+++ {{cookiecutter.project_name}}/core/settings/base.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "core" / "assets",
+    os.path.join(BASE_DIR,'assets')
 ]
 
 STATIC_ROOT = BASE_DIR / 'static'
