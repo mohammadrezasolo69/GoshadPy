b1cc32796b4afda2715e1afa040580713a26d43b
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 17:16:32 +0330

    bugfix

--- {{cookiecutter.project_name}}/core/settings/base.py
+++ {{cookiecutter.project_name}}/core/settings/base.py
@@ -1,11 +1,8 @@
 import os
 import environ
 from pathlib import Path
-
 {%- if cookiecutter.use_i18n == 'y' -%}
-
 from django.utils.translation import gettext_lazy as _
-
 {% endif %}
 
 env = environ.Env()
