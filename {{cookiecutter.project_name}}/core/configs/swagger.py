89fa24caf3197dc73383c34b3c6a8ea0ddbb9a29
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 14:15:27 +0330

    write core/configs/swagger

---
+++ {{cookiecutter.project_name}}/core/configs/swagger.py
@@ -0,0 +1,10 @@
+# Config drf-spectacular
+SPECTACULAR_SETTINGS = {
+    'TITLE': '{{cookiecutter.project_name}}',
+    'DESCRIPTION': '',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR'
+}
