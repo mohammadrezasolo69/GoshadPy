69ab3fe195a7bc9fff93c543b7096d2d9414229b
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 15:45:45 +0330

    bugfix in cors.py

--- {{cookiecutter.project_name}}/core/configs/cors.py
+++ {{cookiecutter.project_name}}/core/configs/cors.py
@@ -3,5 +3,9 @@ from core.settings.base import env
 # Config CORS
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT", ]
+
 # Application definition
-CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS').split(',')
+try:
+    CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS').split(',')
+except:
+    pass
