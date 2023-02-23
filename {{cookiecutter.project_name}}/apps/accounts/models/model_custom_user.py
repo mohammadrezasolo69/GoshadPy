dce0282f3e739eb232277246159c03ee2a51e83a
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 17:13:28 +0330

    bugfix

--- {{cookiecutter.project_name}}/apps/accounts/models/model_custom_user.py
+++ {{cookiecutter.project_name}}/apps/accounts/models/model_custom_user.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
-from core.apps.accounts.manager import CustomUserManager
+from apps.accounts.manager import CustomUserManager
 
 class CustomUser(AbstractUser):
     """ Custom User class  """
