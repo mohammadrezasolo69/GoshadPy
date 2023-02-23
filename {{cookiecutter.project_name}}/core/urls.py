6d94c6bd8e99a1ce180436847a1e0f73354c537f
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-02-23 14:05:07 +0330

    write core/urls.py

--- {{cookiecutter.project_name}}/core/urls.py
+++ {{cookiecutter.project_name}}/core/urls.py
@@ -1,21 +1,41 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+)
+
+urlpatterns_api = [
+
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # my api
+    include('api/v1/', include(urlpatterns_api)),
+
+    # urls drf_spectacular
+    path('schema/', SpectacularAPIView.as_view(api_version='v1'), name='schema'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+
+    {%- if cookiecutter.use_ckeditor == 'y' -%}
+    path('ckeditor/', include('ckeditor_uploader.urls'), name="CKEditor_URL"),# urls CkEditor
+    {%- endif -%}
+
+
+    {%- if cookiecutter.use_django_debug_toolbar == 'y' -%}
+    path('__debug__/', include('debug_toolbar.urls')),# urls debug_toolbar
+    {%- endif -%}
 ]
+
+
+
+# -------------------------------- Config Static and Media File -----------------------------------
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
